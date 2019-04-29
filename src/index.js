003b12bd0e0e12f925d97d0b9385d1c858ee0fee
Daniel Waligora <daniel@blossomeducational.com>
2019-04-29 13:06:53 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// @flow weak
+import {basename, extname} from 'path';
+
+export default function reactElementInfo({types: t}) {
+  const defaultPrefix = 'data-qa';
+  let prefix;
+  let filenameAttr;
+  let nodeNameAttr;
+
+  const visitor = {
+    Program(path, state) {
+      if (state.opts.prefix) {
+        prefix = `data-${state.opts.prefix}`;
+      } else {
+        prefix = defaultPrefix;
+      }
+      filenameAttr = `${prefix}-file`;
+      nodeNameAttr = `${prefix}-node`;
+    },
+
+    JSXOpeningElement(path, state) {
+      const openingElement = path.container.openingElement;
+      const attributes = openingElement.attributes;
+
+      const newAttributes = [];
+      const exludedNames = state.opts.exclude || []
+  
+      if(openingElement.name.name === undefined || exludedNames.indexOf(openingElement.name.name) > -1) {
+        return
+      }
+  
+      if (openingElement.name && openingElement.name.name) {
+        newAttributes.push(t.jSXAttribute(
+          t.jSXIdentifier(nodeNameAttr),
+          t.stringLiteral(openingElement.name.name),
+        ));
+      }
+
+      let name;
+      if (state.file && state.file.opts) {
+        if (state.file.opts.basename) {
+          name = state.file.opts.basename;
+        } else if (state.file.opts.filename) {
+          name = basename(state.file.opts.filename, extname(state.file.opts.filename));
+        }
+      }
+  
+      if (name) {
+        newAttributes.push(t.jSXAttribute(
+          t.jSXIdentifier(filenameAttr),
+          t.stringLiteral(name)
+        ));
+      }
+
+      attributes.push(...newAttributes);
+    },
+  };
+
+  return {
+    visitor,
+  };
+}
